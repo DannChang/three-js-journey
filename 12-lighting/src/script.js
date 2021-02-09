260d75d85497b5f866e66d9c5492586faec914dd
dmagma <dmagma@hotmail.com>
2021-02-08 22:56:12 -0800

    Update debug UI for lighting
    Created all instances of lights available in THREEjs. Able to manipulate its parameters via the debug UI.

--- 12-lighting/src/script.js
+++ 12-lighting/src/script.js
@@ -2,12 +2,14 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
+import { RectAreaLightHelper } from 'three/examples/jsm/helpers/RectAreaLightHelper.js'
 
 /**
  * Base
  */
 // Debug
 const gui = new dat.GUI()
+
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -18,14 +20,171 @@ const scene = new THREE.Scene()
 /**
  * Lights
  */
+
+ // Ambient lighting - omnipresence lighting
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
 scene.add(ambientLight)
 
-const pointLight = new THREE.PointLight(0xffffff, 0.5)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
+//Equavalent code to above:
+ 
+// const ambientLight = new THREE.AmbientLight()
+// ambientLight.color = new THREE.Color(0xffffff)
+// ambientLight.intensity = 0.5
+// scene.add(ambientLight)
+gui
+    .add(ambientLight, 'intensity')
+    .min(0)
+    .max(1)
+    .step(0.001)
+    .name('ambient light intensity')
+
+
+// Directional Lighting -  Rays travelling in parallel (default: above)
+const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.3)
+scene.add(directionalLight)
+    // position rays to middle
+directionalLight.position.set(1, 0.25, 0)
+
+// HemisphereLight - similar to AmbientLight but different color from the sky
+const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 0.3)
+scene.add(hemisphereLight)
+gui
+    .add(hemisphereLight, 'intensity')
+    .min(0)
+    .max(1)
+    .step(0.001)
+    .name('hemisphere light intensity')
+
+// Point light - lights like a lighter; 
+// small light source, and spreads uniformly in every direction
+const pointLight = new THREE.PointLight(0xff9000, 0.5, 10, 2)
 scene.add(pointLight)
+//position light like any object:
+pointLight.position.set(1, - 0.5, 1)
+
+gui
+    .add(pointLight, 'intensity')
+    .min(0)
+    .max(1)
+    .step(0.001)
+    .name('point light intensity')
+gui
+    .add(pointLight, 'distance')
+    .min(0)
+    .max(10)
+    .step(0.1)
+    .name('point light distance')
+gui
+    .add(pointLight, 'decay')
+    .min(0)
+    .max(5)
+    .step(0.01)
+    .name('point light decay')
+
+// RectAreaLight - works like the rectangle light stands you see in photoshoots
+// A mix between a directional light and a diffuse light
+const rectAreaLight = new THREE.RectAreaLight(0x4e00ff, 2, 1, 1)
+scene.add(rectAreaLight)
+
+rectAreaLight.position.set(- 1.5, 0, 1.5)
+rectAreaLight.lookAt(new THREE.Vector3())
+
+gui
+    .add(rectAreaLight, 'intensity')
+    .min(0)
+    .max(5)
+    .step(0.01)
+    .name('rect intensity')
+
+gui
+    .add(rectAreaLight, 'width')
+    .min(0)
+    .max(5)
+    .step(0.01)
+    .name('rect width')
+
+gui
+    .add(rectAreaLight, 'height')
+    .min(0)
+    .max(5)
+    .step(0.01)
+    .name('rect height')
+
+
+// SpotLight - works like a flashlight
+// parameters: color, intensity, distance, angle, penumbra, decay
+const spotLight = new THREE.SpotLight(0x78ff00, 0.5, 10, Math.PI * 0.1, 0.25, 1)
+spotLight.position.set(0, 2, 3)
+scene.add(spotLight)
+
+// moving position of SpotLight to the left of x-axis
+spotLight.target.position.x = - 0.75
+scene.add(spotLight.target)
+
+gui
+    .add(spotLight, 'intensity')
+    .min(0)
+    .max(1)
+    .step(0.0001)
+    .name('spotlight intensity')
+gui
+    .add(spotLight, 'distance')
+    .min(0)
+    .max(100)
+    .step(1)
+    .name('spotlight distance')
+gui
+    .add(spotLight, 'angle')
+    .min(0)
+    .max(Math.PI)
+    .step(Math.PI*0.0001)
+    .name('spotlight angle')
+gui
+    .add(spotLight, 'penumbra')
+    .min(0)
+    .max(1)
+    .step(0.001)
+    .name('spotlight penumbra')
+gui
+    .add(spotLight, 'decay')
+    .min(0)
+    .max(5)
+    .step(0.01)
+    .name('spotlight decay')
+
+
+/**
+ * Helper functions are only available for:
+ *  */    
+//  HemisphereLightHelper
+//  DirectionalLightHelper
+//  PointLightHelper
+//  RectAreaLightHelper ** must import from 'examples' dependency
+//  SpotLightHelper ** must update window after each adjustment
+const hemisphereLightHelper = new THREE.HemisphereLightHelper(hemisphereLight, 0.2)
+scene.add(hemisphereLightHelper)
+
+const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 0.2)
+scene.add(directionalLightHelper)
+
+const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.2)
+scene.add(pointLightHelper)
+
+const rectAreaLightHelper = new RectAreaLightHelper(rectAreaLight)
+scene.add(rectAreaLightHelper)
+window.requestAnimationFrame(() =>
+{
+    rectAreaLightHelper.position.copy(rectAreaLight.position)
+    rectAreaLightHelper.quaternion.copy(rectAreaLight.quaternion)
+    rectAreaLightHelper.update()
+}) // must also be used to update the frames properly
+
+const spotLightHelper = new THREE.SpotLightHelper(spotLight, 0.2)
+scene.add(spotLightHelper)
+window.requestAnimationFrame(() =>
+{
+    spotLightHelper.update()
+})
 
 /**
  * Objects
