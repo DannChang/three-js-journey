07aca5f1017f4968a80edcb547528c6d1bcf4230
dmagma <dmagma@hotmail.com>
2021-02-15 21:43:09 -0800

    Add Box Object
    Box object drops together with sphere and collides

--- 18-physics/src/script.js
+++ 18-physics/src/script.js
@@ -8,9 +8,10 @@ import CANNON from 'cannon'
  * Debug
  */
 const gui = new dat.GUI()
-const debugObject = {}
+const debugSphere = {}
+const debugBox = {}
 
-debugObject.createSphere = () => {
+debugSphere.createSphere = () => {
     createSphere(
         Math.random() * 0.5,
         {
@@ -20,8 +21,24 @@ debugObject.createSphere = () => {
         }
     )
 }
+debugBox.createBox = () => {
+    createBox(
+        Math.random(),
+        Math.random(),
+        Math.random(),
+        {
+            x: (Math.random() - 0.5) * 3,
+            y: 3,
+            z: (Math.random() - 0.5) * 3
+        }
+    )
+}
 
-gui.add(debugObject, 'createSphere')
+gui.add(debugSphere, 'createSphere')
+gui.add(debugBox, 'createBox')
+
+
+
 /**
  * Base
  */
@@ -189,17 +206,18 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const objectsToUpdate = []
 
- const createSphere = (radius, position) => {
-     // Three.js mesh
-     const mesh = new THREE.Mesh(
-         new THREE.SphereBufferGeometry(radius, 20, 20),
-         new THREE.MeshStandardMaterial({
+const sphereGeometry = new THREE.SphereBufferGeometry(1, 20, 20)
+const sphereMaterial = new THREE.MeshStandardMaterial({
             metalness: 0.3,
             roughness: 0.4,
             envMap: environmentMapTexture
         })
-    )
-    mesh.castShadow = true
+
+ const createSphere = (radius, position) => {
+     // Three.js mesh
+     const mesh = new THREE.Mesh(sphereGeometry,sphereMaterial)
+    mesh.scale.set(radius, radius, radius)
+     mesh.castShadow = true
     mesh.position.copy(position)
     scene.add(mesh)
 
@@ -220,8 +238,45 @@ const objectsToUpdate = []
         body
     })
 }
-
 createSphere(0.5, { x: 0, y: 3, z: 0 })
+
+
+// Create Box Object
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
+const boxMaterial = new THREE.MeshStandardMaterial({
+            metalness: 0.3,
+            roughness: 0.4,
+            envMap: environmentMapTexture
+})
+
+ const createBox = (width, height, depth, position) => {
+     // Three.js mesh
+    const mesh = new THREE.Mesh(boxGeometry, boxMaterial)
+    mesh.scale.set(width, height, depth)
+    mesh.castShadow = true
+    mesh.position.copy(position)
+    scene.add(mesh)
+
+    // Cannon.js body
+    const shape = new CANNON.Box(new CANNON.Vec3(width * 0.5, height * 0.5, depth * 0.5))
+    
+    const body = new CANNON.Body({
+        mass: 1,
+        position: new CANNON.Vec3(0, 3, 0),
+        shape: shape,
+        material: defaultMaterial
+    })
+    body.position.copy(position)
+    world.addBody(body)
+
+    // Save in objects to update
+    objectsToUpdate.push({
+        mesh,
+        body
+    })
+}
+
+createBox(1, 1.5, 2, { x: 0, y: 3, z: 0 })
 
 /**
  * Animate
