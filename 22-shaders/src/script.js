9691ee4821ddabc3b70108483c901e820554ff3d
Dan Chang <dmagma@hotmail.com>
2021-02-23 12:51:29 -0800

    Add animation to shader object

--- 22-shaders/src/script.js
+++ 22-shaders/src/script.js
@@ -44,9 +44,19 @@ const material = new THREE.RawShaderMaterial({
     
     vertexShader: testVertexShader,
     fragmentShader: testFragmentShader,
-    transparent: true
+    transparent: true,
+    uniforms:
+    {
+        uFrequency: { value: new THREE.Vector2(10, 5) },
+        uTime: { value: 0 }
+    }
 
 })
+console.log(material)
+
+gui.add(material.uniforms.uFrequency.value, 'x').min(0).max(20).step(0.01).name('frequencyX')
+gui.add(material.uniforms.uFrequency.value, 'y').min(0).max(20).step(0.01).name('frequencyY')
+
 
 // Mesh
 const mesh = new THREE.Mesh(geometry, material)
@@ -104,6 +114,9 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+    
+    // Update material
+    material.uniforms.uTime.value = elapsedTime
 
     // Update controls
     controls.update()
