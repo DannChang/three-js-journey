9b37bc67652ca8bdd387fdb52ffef942208618d8
Dan Chang <dmagma@hotmail.com>
2021-03-02 13:43:56 -0800

    Updated plane position to reflect shadow

--- 26-modified-materials/src/script.js
+++ 26-modified-materials/src/script.js
@@ -141,7 +141,7 @@ depthMaterial.onBeforeCompile = (shader) => {
         `
             #include <begin_vertex>
 
-            float angle = (position.y + uTime) * 0.9;
+            float angle = (position.z + uTime) * 0.9;
             mat2 rotateMatrix = get2dRotateMatrix(angle);
 
             transformed.xz = rotateMatrix * transformed.xz;
