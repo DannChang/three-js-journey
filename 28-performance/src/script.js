58df0d497cd19b25e9fca7dc77c209bd1d46a5e7
dmagma <dmagma@hotmail.com>
2021-03-07 15:24:08 -0800

    Added significant notes on performance module

--- 28-performance/src/script.js
+++ 28-performance/src/script.js
@@ -4,7 +4,8 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import Stats from 'stats.js'
 
 /**
- * Stats GUI
+ * Tip 1 - Monitor the FPS /w Stats GUI
+ * Instantiated here, as well as, in the tick/animate function
  */
 const stats = new Stats()
 stats.showPanel( 0 ); // 0: fps, 1: ms, 2: mb, 3+: custom
@@ -146,9 +147,10 @@ const tick = () =>
     renderer.render(scene, camera)
 
     stats.end()
-
+    
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
+
 }
 
 tick()
@@ -157,15 +159,37 @@ tick()
  * Tips
  */
 
-// // Tip 4
+// // Tip 2 - Disable FPS Limit in Terminal (if you have a powerful computer)
+// // open -a "Google Chrome" --args --disable-gpu-vsync --disable-frame-rate-limit
+
+// // Tip 3 - Monitor Drawcalls (actions of drawing trangles by the GPU)
+// // Monitor draw calls with Spector.js (Chrome extension)
+
+// // Tip 4 - Keep track of # of triangles used for GPU performance
 // console.log(renderer.info)
 
-// // Tip 6
+// // Tip 5 - Good JavaScript code
+// // Use native JavaScript code to keep performances up
+
+// // Tip 6 - Remove objects when necessary
 // scene.remove(cube)
 // cube.geometry.dispose()
 // cube.material.dispose()
 
-// // Tip 10
+// // Tip 7 - Avoid Lights (if possible)
+// // Only use AmbientLight of DirectionalLight if a must
+
+// // Tip 8 - Avoid adding/removing lights
+// // The renderer has to be recompiled again and again
+
+/**
+ * Shadows
+ */
+
+// // Tip 9 - Avoid Shadows 
+// // Instead, find alternatives like Baked Shadows
+
+// // Tip 10 - Optimize Shadow Maps (if using shadows)
 // directionalLight.shadow.camera.top = 3
 // directionalLight.shadow.camera.right = 6
 // directionalLight.shadow.camera.left = - 6
@@ -176,7 +200,7 @@ tick()
 // const cameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
 // scene.add(cameraHelper)
 
-// // Tip 11
+// // Tip 11 - use CastShadow and ReceiveShadow wisely
 // cube.castShadow = true
 // cube.receiveShadow = false
 
@@ -189,11 +213,37 @@ tick()
 // floor.castShadow = false
 // floor.receiveShadow = true
 
-// // Tip 12
+// // Tip 12 - turn Shadow Autoupdate off, but only update shadow once after update
 // renderer.shadowMap.autoUpdate = false
 // renderer.shadowMap.needsUpdate = true
 
-// // Tip 18
+/**
+ * Textures
+ */
+
+// // Tip 13 - Resize Textures
+// // Try to reduce the resolution to the minimum
+
+// // Tip 14 - Keep the resolution to a power of 2
+// // This is important for mipmaps (pyramids are pre-calulated to optimize sequences of images)
+
+// // Tip 15 - Use the right format
+// // Instead of traditional JPG/PNG files, consider .basis files (easier on GPU)
+
+/**
+ * Geometries
+ */
+
+// // Tip 16 - Use BufferGeometries
+// // Non Buffer geometries are bad for performances 
+
+// // Tip 17 - Do not update Vertices
+// // We can do it once when creating the geometry, 
+// // but avoid doing it in the tick/animate function
+
+// // Tip 18 - Mutualize Geometries
+// // If you have multiple Meshes using the same geomtry shape,
+// // create only one geometry, and use it on all the meshes.
 // for(let i = 0; i < 50; i++)
 // {
 //     const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5)
@@ -210,7 +260,7 @@ tick()
 //     scene.add(mesh)
 // }
 
-// // Tip 19
+// // Tip 19 - Merge Geometries
 // for(let i = 0; i < 50; i++)
 // {
 //     const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5)
@@ -227,7 +277,11 @@ tick()
 //     scene.add(mesh)
 // }
 
-// // Tip 20
+/**
+ * Materials
+ */
+
+// // Tip 20 - Mutualize materials
 // const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5)
     
 // for(let i = 0; i < 50; i++)
@@ -244,25 +298,68 @@ tick()
 //     scene.add(mesh)
 // }
 
-// // Tip 22
-// const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5)
+// // Tip 21 - Use cheap materials
+// / E.g. MeshBasicMaterial, MeshLambertMaterial, or MeshPhonMaterial
 
-// const material = new THREE.MeshNormalMaterial()
+/**
+ * Meshes
+ */
+
+// // Tip 22 Use InstancesMesh
+const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5)
+
+const material = new THREE.MeshNormalMaterial()
     
-// for(let i = 0; i < 50; i++)
-// {
-//     const mesh = new THREE.Mesh(geometry, material)
-//     mesh.position.x = (Math.random() - 0.5) * 10
-//     mesh.position.y = (Math.random() - 0.5) * 10
-//     mesh.position.z = (Math.random() - 0.5) * 10
-//     mesh.rotation.x = (Math.random() - 0.5) * Math.PI * 2
-//     mesh.rotation.y = (Math.random() - 0.5) * Math.PI * 2
+for(let i = 0; i < 50; i++)
+{
+    const mesh = new THREE.Mesh(geometry, material)
+    mesh.position.x = (Math.random() - 0.5) * 10
+    mesh.position.y = (Math.random() - 0.5) * 10
+    mesh.position.z = (Math.random() - 0.5) * 10
+    mesh.rotation.x = (Math.random() - 0.5) * Math.PI * 2
+    mesh.rotation.y = (Math.random() - 0.5) * Math.PI * 2
 
-//     scene.add(mesh)
-// }
+    scene.add(mesh)
+}
 
-// // Tip 29
+// // Tip 23 - Low poly
+// // The fewer the polygons, the better the frame rate
+// // If you need details, use normal maps
+
+// // Tip 24 - Use Draco Compression
+// // If the model is complex + detailed, use the Draco compression. 
+// // Must load Draco libraries if using
+
+// // Tip 25 - Gzip
+// // Gzip is a compressino happening on the server side
+
+/**
+ * Cameras
+ */
+
+// // Tip 26 - Field of view
+// // When objects are not in the FOV, they won't be rendered.
+// // Try reducing Camera's FOV for fewer objects, and fewer triangle renders
+
+// // Tip 27 - Near and far
+// // Same idea, but adjust the near and far values where reasonable
+
+/**
+ * Renderer
+ */
+
+// // Tip 29 - Ajdust Pixel Ratio if needed
 // renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+// // Tip 30 - Power preferences
+// // Some devices may be able to switch between different
+// // GPU or different GPU usage. We may specify the powerPreference
+// // property in WebGLRenderer:
+// const renderer = new THREE.WebGLRenderer({
+//     canvas: canvas,
+//     powerPreference: 'high-performance'
+// })
+
 
 // // Tip 31, 32, 34 and 35
 // const shaderGeometry = new THREE.PlaneGeometry(10, 10, 256, 256)
